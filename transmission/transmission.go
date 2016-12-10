9582167469615067519b67cb8183a3221e61375f
Nick Andrew <nick@nick-andrew.net>
2016-12-11 00:46:01 +1100

    Fix repository name in import and godeps

--- transmission/transmission.go
+++ transmission/transmission.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"github.com/longnguyen11288/go-transmission/client"
+	"github.com/lnguyen/go-transmission/client"
 )
 
 //TransmissionClient to talk to transmission
